afd80277c5ad552765a1db113afbf80ac2b87593
small-toolforge <281787160+small-toolforge@users.noreply.github.com>
2026-07-07 04:52:24 +0900

    Bump PWA registration to standalone lab version

--- assets/v0.3/app-5.js
+++ assets/v0.3/app-5.js
@@ -1,9 +1,9 @@
 "use strict";
-const APP_VERSION="v0.3.7";
+const APP_VERSION="v0.3.8";
 
 if ("serviceWorker" in navigator && location.protocol.indexOf("http") === 0) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js?v=0.3.7").catch(() => {});
+    navigator.serviceWorker.register("./sw.js?v=0.3.8").catch(() => {});
     let reloaded = false;
     navigator.serviceWorker.addEventListener("controllerchange", () => {
       if (reloaded) return;
