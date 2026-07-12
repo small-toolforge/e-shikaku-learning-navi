1a01db7dcedf8ef7772a0ed57084cfd9791396a8
small-toolforge <281787160+small-toolforge@users.noreply.github.com>
2026-07-12 18:07:15 +0900

    chore: 開発・運用環境カードをオフライン対象へ追加

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* E資格 学習ナビ - Service Worker v0.4.0 development */
-const CACHE_NAME="eshikaku-atlas-v0.4.0-dev8";
+const CACHE_NAME="eshikaku-atlas-v0.4.0-dev9";
 const ASSETS=[
   "./",
   "./index.html",
@@ -15,6 +15,7 @@ const ASSETS=[
   "./assets/v0.4.0/cards/cards-02-machine-learning.js",
   "./assets/v0.4.0/cards/cards-03-deep-learning-base.js",
   "./assets/v0.4.0/cards/cards-04-deep-learning-application.js",
+  "./assets/v0.4.0/cards/cards-05-development-operations.js",
   "./assets/v0.4.0/questions/questions-01-math.js",
   "./assets/v0.4.0/questions/questions-02-machine-learning.js",
   "./assets/v0.4.0/questions/questions-03-deep-learning-base.js",
